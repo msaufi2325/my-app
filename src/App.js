db75ad33cf57a2ef88bf9486d2832ccdb456d3ec
Saufi <msaufi2325@gmail.com>
2024-05-09 23:03:20 +0900

    Refactor code to use functional components with props for dynamic message and add toggle functionality to HelloWorld component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./App.css";
 
 function HelloWorld(props) {
   const [isTrue, setIsTrue] = useState(true);
+  const [crowd, setCrowd] = useState([]);
 
   const toggleTrue = () => {
     setIsTrue(!isTrue);
   };
+
+  useEffect(() => {
+    console.log("useEffect fired!");
+
+    let people = [
+      {
+        id: 1,
+        firstName: "Mary",
+        lastName: "Jones",
+        dob: "1997-05-02",
+      },
+      {
+        id: 2,
+        firstName: "Jack",
+        lastName: "Smith",
+        dob: "1999-02-04",
+      },
+    ];
+    setCrowd(people);
+  }, []);
 
   return (
     <Fragment>
@@ -25,6 +46,14 @@ function HelloWorld(props) {
         Toggle isTrue
       </a>
       <hr />
+      <h3>People</h3>
+      <ul className="list-group">
+        {crowd.map((person) => (
+          <li key={person.id} className="list-group-item">
+            {person.firstName} {person.lastName} born on {person.dob}
+          </li>
+        ))}
+      </ul>
     </Fragment>
   );
 }
