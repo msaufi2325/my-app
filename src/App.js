08ccc88caf9411966e3a8f4ade9b7309c7fce6af
Saufi <msaufi2325@gmail.com>
2024-05-09 23:13:34 +0900

    Refactor code to use functional components with props for dynamic message and add form inputs for user details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 function HelloWorld(props) {
   const [isTrue, setIsTrue] = useState(true);
   const [crowd, setCrowd] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dob, setDob] = useState("");
 
   const toggleTrue = () => {
     setIsTrue(!isTrue);
@@ -45,6 +48,27 @@ function HelloWorld(props) {
       <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>
         Toggle isTrue
       </a>
+      <hr />
+      <form autoComplete="off">
+        <div className="mb-3">
+          <label className="form-label" htmlFor="first-name">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="first-name"
+            id="first-name"
+            autoComplete="first-name-new"
+            className="form-control"
+            onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+        </div>
+      </form>
+      <div>
+        First Name: {firstName} <br />
+        Last Name: {lastName} <br />
+        Date of Birth: {dob} <br />
+      </div>
       <hr />
       <h3>People</h3>
       <ul className="list-group">
