2cc41848bba1debb57fc75db8e0c450560684d5b
Saufi <msaufi2325@gmail.com>
2024-05-09 19:58:30 +0900

    Refactor code to use CSS class for red heading

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./AppClass.css";
 
 export default class AppClass extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, World!</h1>
+        <h1 className="h1-red">Hello, World!</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppClass from "./AppClass";
-import HelloWorld from "./Hello";
+import HelloWorld from "./HelloWorld";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
