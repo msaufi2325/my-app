e49157af2ae4af69eb79970273637085fe467372
Saufi <msaufi2325@gmail.com>
2024-05-09 19:53:44 +0900

    Add HelloWorld component to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppClass from "./AppClass";
+import HelloWorld from "./Hello";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppClass />
+    <HelloWorld />
   </React.StrictMode>
 );
