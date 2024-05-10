1a1845b852972ef5bd886dbae6f435fe4d6c80c3
Saufi <msaufi2325@gmail.com>
2024-05-10 22:30:17 +0900

    Refactor code to use functional components, add form inputs for user details, and update AppClass component

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from "react";
 import "./AppClass.css";
+import Input from "./Input";
 
 export default class AppClass extends Component {
   constructor(props) {
@@ -13,6 +14,28 @@ export default class AppClass extends Component {
       isTrue: false,
       crowd: [],
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      firstName: "",
+      lastName: "",
+      dob: "",
+      crowd: [
+        {
+          id: 1,
+          firstName: "Mary",
+          lastName: "Jones",
+          dob: "1997-05-02",
+        },
+        {
+          id: 2,
+          firstName: "Jack",
+          lastName: "Smith",
+          dob: "1999-02-04",
+        },
+      ],
+    });
   }
 
   toggleTrue = () => {
@@ -41,6 +64,65 @@ export default class AppClass extends Component {
         >
           Toggle isTrue
         </a>
+
+        <hr />
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="first-name">
+              First Name
+            </label>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              ref={this.firstNameRef}
+              autoComplete="first-name-new"
+              className="form-control"
+              onChange={(e) => this.setFirstName(e.target.value)}
+            ></input>
+          </div>
+
+          <Input
+            title="Last Name"
+            type="text"
+            ref={this.lastNameRef}
+            name="last-name"
+            autoComplete="last-name-new"
+            className="form-control"
+            onChange={(e) => this.setState({ lastName: e.target.value })}
+          ></Input>
+
+          <Input
+            title="Date of Birth"
+            type="date"
+            ref={this.dobRef}
+            name="dob"
+            autoComplete="dob-new"
+            className="form-control"
+            onChange={(e) => this.setState({ dob: e.target.value })}
+          ></Input>
+
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-primary"
+          ></input>
+        </form>
+        <div>
+          First Name: {this.state.firstName} <br />
+          Last Name: {this.state.lastName} <br />
+          Date of Birth: {this.state.dob} <br />
+        </div>
+
+        <hr />
+        <h3>People</h3>
+        <ul className="list-group">
+          {this.state.crowd.map((person) => (
+            <li key={person.id} className="list-group-item">
+              {person.firstName} {person.lastName} born on {person.dob}
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
